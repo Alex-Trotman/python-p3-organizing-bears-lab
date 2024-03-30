d398f3f1b5bba1b5add94832587eb776e55e4059
Alex <alextrotman04@gmail.com>
2024-03-30 15:51:53 -0500

    save

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "M", "Blue", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "F", "Blue", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "M", "Blue", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "F", "Blue", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "M", "Blue", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "F", "Blue", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "M", "Blue", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 23, "M", "Blue", "Rowdy", 1);
